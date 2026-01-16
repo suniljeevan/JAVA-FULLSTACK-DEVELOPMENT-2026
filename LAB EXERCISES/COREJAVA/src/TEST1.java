5121b06f6691ccbd10fdf5803355adff39c0674d
suniljeevan <36226091+suniljeevan@users.noreply.github.com>
2026-01-16 15:30:58 +0530

    TEST1

--- LAB EXERCISES/COREJAVA/src/TEST1.java
+++ LAB EXERCISES/COREJAVA/src/TEST1.java
@@ -1,8 +1,51 @@
+// Demonstrate class-object
+// Demonstrate two-class principle
+// Demonstrate S from SOLID Principle
+// Encapsulation, Abstraction, Data hiding
+// server classes 
+import java.util.Scanner;
+class Student {
+	// instance variables
+	int rollno;
+	String name;
+	double cgpa;
+  	// public methods as codes
+	// setter getter  business methods
+	// alt shift s
+	public int getRollno() {
+		return rollno;
+	}
+	public void setRollno(int rollno) {
+		this.rollno = rollno;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public double getCgpa() {
+		return cgpa;
+	}
+	public void setCgpa(double cgpa) {
+		this.cgpa = cgpa;
+	}
+	// method to update cgpa
+	public void updateCgpa() {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter new cgpa");
+		double newcgpa=sc.nextDouble();
+		this.cgpa=newcgpa;
+		System.out.println("CGPA Updated");
+	}
+	
+}
+//client class 
+
+
 
 public class TEST1 {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
 	}
 
